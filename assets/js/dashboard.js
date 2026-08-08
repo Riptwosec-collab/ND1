594849e539eb5c340fce267345896706f7efd4b5
Riptwosec-collab <aidsaras@gmail.com>
2026-08-08 23:00:14 +0700

    Switch Home to v29 SVG image background

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -7,7 +7,8 @@ const LEGACY_HOME_STYLE_IDS = [
   'home-balance-v22-styles',
   'home-reference-v23-styles',
   'home-command-v24-styles',
-  'home-epic-v27-styles'
+  'home-epic-v27-styles',
+  'home-image-v28-styles'
 ];
 
 export function initDashboard() {
@@ -34,8 +35,8 @@ function removeLegacyHomeStyles() {
 }
 
 function ensureHomeStylesheet() {
-  const stylesheetId = 'home-image-v28-styles';
-  const stylesheetHref = './assets/css/home-image-v28.css?v=20260808-28';
+  const stylesheetId = 'home-image-v29-styles';
+  const stylesheetHref = './assets/css/home-image-v29.css?v=20260808-29';
   const existing = document.getElementById(stylesheetId);
 
   if (existing) {
