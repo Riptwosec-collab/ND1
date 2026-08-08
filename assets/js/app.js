184e222271fea373bc1381956e944c59f7b0edd5
Riptwosec-collab <aidsaras@gmail.com>
2026-08-08 22:31:47 +0700

    Bump home image background runtime version

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@ import { getSettings, initStorage, saveSettings } from './storage.js';
 import { initToast } from './toast.js';
 import { initModal } from './modal.js';
 import { initRouter, navigate } from './router.js';
-import { initDashboard } from './dashboard.js?v=20260808-27';
+import { initDashboard } from './dashboard.js?v=20260808-28';
 import { initLinks } from './links.js';
 
 function runInitializer(name, initializer) {
